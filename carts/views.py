40c2f59f7160bb5bb788b7788e2f9da3d1687a8d
datvu <datnguyen@emotiv.com>
2024-09-25 17:39:58 +0700

    .

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItems
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
@@ -12,14 +12,22 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    product_variation = []
     if request.method == 'POST':
         for item in request.POST:
             key = item 
             value = request.POST[key]
+            print(key, value, request, request)
 
-            print(key,value)
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+                print(variation)
+            except Exception as e:
+                print(e)
+                pass
     
-    product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
